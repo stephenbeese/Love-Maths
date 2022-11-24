090018064b98657e203f1d7747c47e97c903afaf
stephenbeese <stephenbeese1@live.co.uk>
2022-11-24 14:41:04 +0000

    Add function to calculate correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,8 +39,22 @@ function runGame(gameType) {
 function checkAnswer() {
 
 }
-
+/**
+ * Get the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer(){
+    
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator ==="+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`)
+        throw `Unimplemented operator ${operator} Aborting!`
+    }
 
 }
 
