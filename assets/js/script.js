9ba3183983909ae7c7a43b93305488bbdb9194f7
stephenbeese <stephenbeese1@live.co.uk>
2022-11-25 14:40:31 +0000

    Add division game code in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,11 +43,14 @@ function runGame(gameType) {
         displayMultipyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
 }
+
 /**
  * Checks the answer against the first element in
  * the returned calculateCorrectAnswer array.
@@ -85,6 +88,8 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator} Aborting!`
@@ -128,5 +133,12 @@ function displayMultipyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    let result = operand1 * operand2;
+    document.getElementById("operand1").textContent = result;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
 
